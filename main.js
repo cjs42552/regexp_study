99eec4177c31fdb2340de7dbb1a165c2a4e38110
cjs42552 <57180193+cjs42552@users.noreply.github.com>
2021-07-01 16:52:32 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ cjs42552@naver.com
 https://regexr.com/
 The quick brown fox jumps over the lazy dog.
 abbcccdddd
+http://localhost:1234
+동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세.
 `
 
 // const regexp = new RegExp('the', 'gi');
@@ -28,3 +30,32 @@ console.log(str);
 console.log(str.match(/the/gi));
 
 console.log(str.match(/\.$/gim));
+
+// 패턴 (표현1)
+
+console.log(str.match(/d$/gm));
+console.log(str.match(/^t/gim));
+console.log(str.match(/h..p/g));
+console.log(str.match(/jumps|lazy/g));
+console.log(str.match(/https?/g));
+console.log(str.match(/d{2}/g));
+console.log(str.match(/d{2,}/g));
+console.log(str.match(/\b\w{2,3}\b/g));
+// \w -> 숫자를 포함한 영어 알파벳 문자 \b -> 문자 경계
+
+// 패턴 (표현2)
+console.log(str.match(/[dog]/g));
+console.log(str.match(/[0-9]/g));
+console.log(str.match(/[0-9]{1,}/g));
+console.log(str.match(/[가-힣]{1,}/g));
+console.log(str.match(/\bj\w{1,}\b/g));
+console.log(str.match(/\d/g));
+
+const h = ` the hello world        !
+
+`
+console.log(h.match(/\s/g));
+console.log(h.replace(/\s/g, ''));
+
+console.log(str.match(/.{1,}(?=@)/g));
+console.log(str.match(/(?<=@).{1,}/g));
